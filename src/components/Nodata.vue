64ee4bb96b8620fdcd77a123e2980d05549f0df3
shyshyHL <925843129@qq.com>
2023-12-10 22:41:59 +0800

    web

--- src/components/Nodata.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="w-full h-80 flex items-center justify-center">
-    <el-empty v-if="true" description=" " class="mt-10" />
-  </div>
-</template>
- 
--- src/lang/jp.js
+++
@@ -1,4 +0,0 @@
-// 日文  key由 文字中的四个字的拼音组成
-export const lang = {
-
-}
