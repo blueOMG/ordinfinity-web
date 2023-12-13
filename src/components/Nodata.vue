9e25c478e07b4eb376ccde9de48817ede7db611c
shyshyhl <925843129@qq.com>
2023-12-13 11:24:23 +0800

    bug

--- src/components/Nodata.vue
+++ src/components/Nodata.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="w-full h-80 flex items-center justify-center">
-    <el-empty v-if="true" description=" " class="mt-10" />
+  <div class="w-full h-80 flex flex-col items-center justify-center">
+    <img
+      src="../assets/img/cross/nodata.png"
+      class="lg:[4.38rem] mb-4"
+      alt=""
+    />
+    <span class="text-[1.13rem] lg:text-[0.88rem] text-[#9E9E9E]">No Data</span>
   </div>
 </template>
  
--- src/views/crosschain/dialog/linkwallet.vue
+++ src/views/crosschain/dialog/linkwallet.vue
@@ -1,79 +1,101 @@
 <template>
-  <div class="w-[90vw] lg:w-[34rem]  bg-[#1A1B1D] rounded-[1.67rem] lg:rounded-[1.25rem] lg:ml-[16.5rem]">
-    <div class="px-[1.25rem] py-[1rem] lg:px-[1.88rem] lg:py-[1.3rem] border-b-[1px] border-[#272727] flex items-center justify-between">
-      <span class="text-white text-[2rem] lg:text-[1.88rem] font-bold">Connect Wallet</span>
-      <img src="../../../assets/img/cross/close.png" alt="" class="w-[1.67rem] lg:w-[1.5rem] cursor-pointer" @click="$emit('close')"/>
+  <div
+    class="w-[90vw] lg:w-[34rem] bg-[#1A1B1D] rounded-[1.67rem] lg:rounded-[1.25rem] lg:ml-[16.5rem]"
+  >
+    <div
+      class="px-[1.25rem] py-[1rem] lg:px-[1.88rem] lg:py-[1.3rem] border-b-[1px] border-[#272727] flex items-center justify-between"
+    >
+      <span class="text-white text-[2rem] lg:text-[1.88rem] font-bold"
+        >Connect Wallet</span
+      >
+      <img
+        src="../../../assets/img/cross/close.png"
+        alt=""
+        class="w-[1.67rem] lg:w-[1.5rem] cursor-pointer"
+        @click="$emit('close')"
+      />
     </div>
     <div class="p-[1.67rem] lg:p-[1.87rem] flex">
-      <div 
+      <div
         @click="link('sat')"
-      class="w-[9rem] h-[9rem] lg:w-[8rem] lg:h-[8rem] rounded-[1.5rem] lg:rounded-[1rem] flex flex-col items-center justify-center mr-[1.6rem] lg:mr-[1.87rem] bg-[#212223] cursor-pointer">
-        <img src="../../../assets/img/cross/sat.png" class="w-[3rem] lg:w-[2.5rem]" alt="" />
-        <span class="mt-[0.33rem] text-white text-[1.2rem] font-bold lg:text-[1rem]">UniSat Wallet</span>
+        class="w-[9rem] h-[9rem] lg:w-[8rem] lg:h-[8rem] rounded-[1.5rem] lg:rounded-[1rem] flex flex-col items-center justify-center mr-[1.6rem] lg:mr-[1.87rem] bg-[#212223] cursor-pointer"
+      >
+        <img
+          src="../../../assets/img/cross/sat.png"
+          class="w-[3rem] lg:w-[2.5rem]"
+          alt=""
+        />
+        <span
+          class="mt-[0.33rem] text-white text-[1.2rem] font-bold lg:text-[1rem]"
+          >UniSat Wallet</span
+        >
       </div>
-      <div 
-      @click="link('okx')"
-      class="w-[9rem] h-[9rem] lg:w-[8rem] lg:h-[8rem] rounded-[1.5rem] lg:rounded-[1rem] flex flex-col items-center justify-center mr-[1.6rem] lg:mr-[1.87rem] bg-[#212223] cursor-pointer">
-        <img src="../../../assets/img/cross/okx.png" class="w-[3rem] lg:w-[2.5rem]" alt="" />
-        <span class="mt-[0.33rem] text-white text-[1.2rem] font-bold lg:text-[1rem]">Xverse Wallet</span>
+      <div
+        @click="link('okx')"
+        class="w-[9rem] h-[9rem] lg:w-[8rem] lg:h-[8rem] rounded-[1.5rem] lg:rounded-[1rem] flex flex-col items-center justify-center mr-[1.6rem] lg:mr-[1.87rem] bg-[#212223] cursor-pointer"
+      >
+        <img
+          src="../../../assets/img/cross/okx.png"
+          class="w-[3rem] lg:w-[2.5rem]"
+          alt=""
+        />
+        <span
+          class="mt-[0.33rem] text-white text-[1.2rem] font-bold lg:text-[1rem]"
+          >Xverse Wallet</span
+        >
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-import utils from '../../../utils/utils'
+import utils from "../../../utils/utils";
 export default {
   data() {
-    return {
-      
-    }
+    return {};
   },
-  mounted() {
-    
-  },
+  mounted() {},
   methods: {
     link(type) {
-      if(type=='sat') {
-        this.linkSat()
+      if (type == "sat") {
+        this.linkSat();
       } else {
-        this.linkOkx()
+        this.linkOkx();
       }
     },
     async linkSat() {
-      if (typeof window.unisat !== 'undefined') {
-        EElNotification.error('Not found unisat wallet')
-        return 
+      if (typeof window.unisat == "undefined") {
+        ElNotification.error("Not found unisat wallet");
+        return;
       }
       try {
         let accounts = await window.unisat.requestAccounts();
-        localStorage.setItem('WALLETTYPE','sat');
-        this.$store.commit('setUseraddress',accounts[0]);
-        this.$emit('close')
+        localStorage.setItem("WALLETTYPE", "sat");
+        this.$store.commit("setUseraddress", accounts[0]);
+        this.$emit("close");
         utils.accountChange();
       } catch (e) {
-        console.log('connect failed');
+        console.log("connect failed");
       }
     },
     async linkOkx() {
-      if(!window.okxwallet) {
-        ElNotification.error('Not found OKX wallet')
-        return 
+      if (!window.okxwallet) {
+        ElNotification.error("Not found OKX wallet");
+        return;
       }
       try {
-        const accounts = await okxwallet.request({ method: 'eth_requestAccounts' });
-        console.log(accounts)
-        localStorage.setItem('WALLETTYPE','okx');
-        this.$store.commit('setUseraddress',accounts[0]);
-        this.$emit('close')
+        const accounts = await okxwallet.request({
+          method: "eth_requestAccounts",
+        });
+        console.log(accounts);
+        localStorage.setItem("WALLETTYPE", "okx");
+        this.$store.commit("setUseraddress", accounts[0]);
+        this.$emit("close");
         utils.accountChange();
-      } catch (error) {
-        
-      }
-    }
-  }
-}
+      } catch (error) {}
+    },
+  },
+};
 </script>
 <style scoped>
 </style>
